0a420d3a21f30243dd00ba058d073dd5060d8d87
TripleNine666 <kolumb1709@gmail.com>
2023-02-02 21:29:53 +0300

    ResourceItem component

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul>
+    <resource-item
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></resource-item>
+  </ul>
+</template>
+
+<script>
+import ResourceItem from "./components/Resources/ResourceItem.vue";
+export default {
+  components: {
+    ResourceItem,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: "official-guide",
+          title: "Oficial guide",
+          description: "The oficial vue.js documintation",
+          link: "https://vuejs.org",
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "Learn to google...",
+          link: "https://google.com",
+        },
+      ],
+    };
+  },
+};
+</script>
